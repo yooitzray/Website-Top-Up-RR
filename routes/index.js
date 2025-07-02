c0accf94ed1c672b142f453e77fc73a555db545c
RuZ025 <rubensandi99@gmail.com>
2025-07-02 15:23:18 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,10 @@
-const express = require('express');
-const router = express.Router();
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 
-router.get('/', (req, res) => {
-  res.render('index'); // homepage
-});
-
-router.get('/topup', (req, res) => {
-  res.render('topup'); // form top up
-});
-
-router.post('/topup', (req, res) => {
-  const { userId, game, nominal, payment } = req.body;
-  // logika simpan pesanan atau redirect pembayaran
-  res.send(`Top up ${nominal} untuk ${game} berhasil!`);
-});
-
-module.exports = router;
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  )
+}
